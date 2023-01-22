bbc999b1cbbdb61868edf4067e68dfc63efddeef
Enes Kaan Bilir <bilireneskaan@gmail.com>
2023-01-22 19:01:20 +0300

    feat:DOM,metod,proporety,event

---
+++ sayfa1.js
@@ -0,0 +1,15 @@
+let el1  = document.querySelector("#kutu1")
+
+let el2  = document.querySelector("#kutu2")
+
+console.log(el1.id)
+el1.style.backgroundColor = "red"
+el1.style.minHeight ="50px"
+
+el1.addEventListener( "click" , function() {  
+    console.log("kutu1 tıklandı")
+}  )
+
+let sinifVarmi = el1.hasAttribute("class")
+
+console.log(sinifVarmi)
